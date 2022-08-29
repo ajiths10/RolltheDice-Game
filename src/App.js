0c505fd4f034829da6e7bd8d30effa580a1e8ad2
ajiths10 <ajithsnair10@gmail.com>
2022-08-30 00:07:11 +0530

    Winner announce added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   const [diceOne, setDiceOne] = useState(1);
   const [diceTwo, setDiceTwo] = useState(1);
-
+  const [initialStart, setInitialStart] = useState(false);
   const randomNumber = () => {
     return Math.floor(Math.random() * 6) + 1;
   };
@@ -12,12 +12,21 @@ const App = () => {
   const handleRoll = () => {
     setDiceOne(randomNumber());
     setDiceTwo(randomNumber());
+    setInitialStart(true);
   };
 
   return (
     <div className="App">
       <div className="heading-conatiner">
-        <h1>Lets Play</h1>
+        <h1>
+          {!initialStart
+            ? "Lets Play"
+            : diceOne > diceTwo
+            ? "Player 1 Wins"
+            : diceOne === diceTwo
+            ? "Math tie"
+            : "Player 2 Wins"}
+        </h1>
       </div>
       <div className="Dice-container">
         <div>
@@ -33,7 +42,7 @@ const App = () => {
           <img
             src={require(`./Common/Images/dice${diceTwo}.png`)}
             className="Dice-logo"
-            alt="dice1"
+            alt="dice2"
           />
         </div>
       </div>
