7c09b29de263fd70183ac72c50e6fc41a4192c73
ajiths10 <ajithsnair10@gmail.com>
2022-08-29 23:34:37 +0530

    Add basic design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import logo from './logo.svg';
-import dice1 from './Common/Images/dice1.png'
-import dice2 from './Common/Images/dice2.png'
-import dice3 from './Common/Images/dice3.png'
-import dice4 from './Common/Images/dice4.png'
-import dice5 from './Common/Images/dice5.png'
-import dice6 from './Common/Images/dice6.png'
-import './App.css';
+import logo from "./logo.svg";
+import dice1 from "./Common/Images/dice1.png";
+import dice2 from "./Common/Images/dice2.png";
+import dice3 from "./Common/Images/dice3.png";
+import dice4 from "./Common/Images/dice4.png";
+import dice5 from "./Common/Images/dice5.png";
+import dice6 from "./Common/Images/dice6.png";
+import "./App.css";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">     
-        <img src={dice1} className="App-logo" alt="logo" />     
+    <div className="App">
+      <div className="heading-conatiner">
+        <h1>Lets Play</h1>
+      </div>
+      <div className="Dice-container">
+        <div>
+          <h3>Player 1</h3>
+          <img src={dice1} className="Dice-logo" alt="dice1" />
+        </div>
+        <div>
+          <h3>Player 2</h3>
+          <img src={dice1} className="Dice-logo" alt="dice1" />
+        </div>
+      </div>
+      <div>
+        <button className="RollButton">Roll the Dice</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
