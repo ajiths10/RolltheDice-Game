a8eab67697e40580a6a514b3909c9a738f6a367d
ajiths10 <ajithsnair10@gmail.com>
2022-09-04 20:44:11 +0530

    Bug Fix

--- src/Common/component/PartyPops.js
+++ src/Common/component/PartyPops.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Confetti from "react-confetti";
-// import  useWindowSize  from 'react-use-window-size'
+import useWindowSize from "react-use/lib/useWindowSize";
 
 const PartyPops = ({ action }) => {
-  //   const { width, height } = useWindowSize()
-  return action ? <Confetti /> : null;
+  const { width, height } = useWindowSize();
+  return action ? <Confetti width={width} height={height} /> : null;
 };
 
 export default PartyPops;
