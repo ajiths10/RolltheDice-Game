e296cf003c9626b1872a818fb7d3156d234418cf
ajiths10 <ajithsnair10@gmail.com>
2022-09-05 21:59:17 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,99 +1,21 @@
 import React, { useEffect, useState } from "react";
-import DiceComponent from "./component/Dice/DiceComponent";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import Button from "@mui/material/Button";
 import "./App.css";
 import FormComponent from "./component/formComponent";
-import winner from "./Common/Images/winner.png";
-import PartyPops from "./component/common/PartyPops";
 
 const App = () => {
-  const [diceOne, setDiceOne] = useState(1);
-  const [diceTwo, setDiceTwo] = useState(1);
-  const [initialStart, setInitialStart] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
   const [formikValues, setFormikValues] = useState("");
-  const [isPartyPopup, setPartyPopup] = useState(false);
-
-  const randomNumber = () => {
-    return Math.floor(Math.random() * 6) + 1;
-  };
-
-  const handleRoll = () => {
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-      setDiceOne(randomNumber());
-      setDiceTwo(randomNumber());
-      setInitialStart(false);
-    }, 1000);
-  };
 
   const fieldFunction = (payload) => {
     setFormikValues(payload);
   };
 
-  useEffect(() => {
-    if (initialStart) return setPartyPopup(false);
-    if (diceOne === diceTwo) return setPartyPopup(false);
-    return setPartyPopup(true);
-  }, [initialStart, diceOne, diceTwo]);
-
-  return (
-    <div className="App">
-      <PartyPops action={isPartyPopup} />
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={isLoading}
-        // onClick={handleClose}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      <FormComponent fieldFunction={fieldFunction} />
-      <div className="heading-conatiner">
-        <h1>
-          {initialStart ? (
-            "Lets Play"
-          ) : diceOne > diceTwo ? (
-            <>
-              <img src={winner} className="winner" alt="winnerlogo" />
-              &nbsp; {formikValues.player1.toUpperCase()} Wins &nbsp;
-              <img src={winner} className="winner" alt="winnerlogo" />
-            </>
-          ) : diceOne === diceTwo ? (
-            "Math Tie"
-          ) : (
-            <>
-              <img src={winner} className="winner" alt="winnerlogo" />
-              &nbsp; {formikValues.player2.toUpperCase()} Wins &nbsp;
-              <img src={winner} className="winner" alt="winnerlogo" />
-            </>
-          )}
-        </h1>
-      </div>
-      <div className="Dice-container">
-        <>
-          <DiceComponent
-            dice={diceOne}
-            playerLabel={formikValues.player1}
-            key={1}
-          />
-          <DiceComponent
-            dice={diceTwo}
-            playerLabel={formikValues.player2}
-            key={2}
-          />
-        </>
-      </div>
-      <div>
-        <button className="RollButton" onClick={handleRoll}>
-          Roll the Dice
-        </button>
-      </div>
-    </div>
-  );
+  return <div className="App">
+     <FormComponent fieldFunction={fieldFunction} formikValues={formikValues}/>
+     
+  </div>;
 };
 
 export default App;
--- src/component/Dice/index.js
+++ src/component/Dice/index.js
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DiceComponent from "./DiceComponent";
+import winner from "../../Common/Images/winner.png";
+import PartyPops from "../common/PartyPops";
+import "./dice.css";
 
-const DiceGame = ( ) => {
+const DiceGame = () => {
+  const [diceOne, setDiceOne] = useState(1);
+  const [diceTwo, setDiceTwo] = useState(1);
+  const [initialStart, setInitialStart] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isPartyPopup, setPartyPopup] = useState(false);
 
-}
+  const randomNumber = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
+  const handleRoll = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      setDiceOne(randomNumber());
+      setDiceTwo(randomNumber());
+      setInitialStart(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (initialStart) return setPartyPopup(false);
+    if (diceOne === diceTwo) return setPartyPopup(false);
+    return setPartyPopup(true);
+  }, [initialStart, diceOne, diceTwo]);
+
+  return (
+    <div className="App">
+      <PartyPops action={isPartyPopup} />
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isLoading}
+        // onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+
+      <div className="heading-conatiner">
+        <h1>
+          {initialStart ? (
+            "Lets Play"
+          ) : diceOne > diceTwo ? (
+            <>
+              <img src={winner} className="winner" alt="winnerlogo" />
+              &nbsp; {props.formikValues.player1.toUpperCase()} Wins &nbsp;
+              <img src={winner} className="winner" alt="winnerlogo" />
+            </>
+          ) : diceOne === diceTwo ? (
+            "Math Tie"
+          ) : (
+            <>
+              <img src={winner} className="winner" alt="winnerlogo" />
+              &nbsp; {props.formikValues.player2.toUpperCase()} Wins &nbsp;
+              <img src={winner} className="winner" alt="winnerlogo" />
+            </>
+          )}
+        </h1>
+      </div>
+      <div className="Dice-container">
+        <>
+          <DiceComponent
+            dice={diceOne}
+            playerLabel={props.formikValues.player1}
+            key={1}
+          />
+          <DiceComponent
+            dice={diceTwo}
+            playerLabel={props.props.formikValues.player2}
+            key={2}
+          />
+        </>
+      </div>
+      <div>
+        <button className="RollButton" onClick={handleRoll}>
+          Roll the Dice
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DiceGame;
